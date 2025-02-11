45a79cb1aa890b0cf0a97d6c26a4059ce87c43ac
Scott Wynn <scott@wynnhome.com>
2025-02-11 09:46:05 -0700

    modified:   terraform-manifests/07-aks-cluster.tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -11,8 +11,8 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name       = "systempool"
     vm_size    = "Standard_DS2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    zones   = [1, 2, 3]
-    auto_scaling_enabled = true
+    #zones   = [1, 2, 3]
+    #auto_scaling_enabled = true
     max_count            = 3
     min_count            = 1
     os_disk_size_gb      = 30
